659579df852cf7fc24c737bacd4e5e66d73c89f7
Jordan <jordgreer@gmail.com>
2023-06-25 08:52:46 -0400

    We do a little more cleanup

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -1,33 +1,29 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+contract ManualToken {
+    mapping(address => uint256) public s_balances;
 
-contract mManualToken {
-
-    mapping(address => uint256) s_balances;
-
-
-    function name() public view returns (string memory){
+    function name() public pure returns (string memory) {
         return "Manual Token";
     }
 
-    function decimals() public pure returns (uint8){
+    function decimals() public pure returns (uint8) {
         return 18;
-    }   
+    }
 
-    function totalSupply() public pure returns (uint256){
+    function totalSupply() public pure returns (uint256) {
         return 100 ether; // 100 * 10^18
     }
 
-    function balanceOf(address owner) public view returns (address owner){
-        return s_balances[owner];
+    function balanceOf(address _owner) public view returns (uint256) {
+        return s_balances[_owner];
     }
 
-    function transfer(address 256 _to, uint256 _amount) public {
+    function transfer(address _to, uint256 _amount) public {
         uint256 previousBalances = balanceOf(msg.sender) + balanceOf(_to);
         s_balances[msg.sender] -= _amount;
         s_balances[_to] += _amount;
         require(balanceOf(msg.sender) + balanceOf(_to) == previousBalances);
     }
-
 }
